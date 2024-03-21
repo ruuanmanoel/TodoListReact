04d34f46a1cff8e67b61fafafa947ed5776ee7d7
Ruan Manoel <ruuanmanoel@gmail.com>
2024-03-21 20:45:28 -0300

    Feature: Adicionada funções para tratar e criar todo

--- todo_list/src/components/TodoForm.jsx
+++ todo_list/src/components/TodoForm.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react"
 
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
+    const [ value, setValue] = useState("")
+    const [ category, setCategory] = useState("")
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        if(!value || !category) return;
+        console.log(value, category)
+        addTodo(value,category)
+        setValue("")
+        setCategory("")
+    }
   return (
     <div className="todo-form">
         <h2>Criar Tarefa</h2>
-        <form>
-            <input type="text" placeholder="Digite o Título" />
-            <select>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder="Digite o Título" 
+                value={value}
+                onChange={(e) => setValue(e.target.value)} 
+            />
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
                 <option value="">Selecione um categoria</option>
                 <option value="Trabalho">Trabalho</option>
                 <option value="Pessoal">Pessoal</option>
