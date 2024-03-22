8a66c6eb6c53b69fad187f146387ae0552bcd402
Ruan Manoel <ruuanmanoel@gmail.com>
2024-03-21 21:04:15 -0300

    Feature: Completar tarefa

--- todo_list/src/components/Todo.jsx
+++ todo_list/src/components/Todo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Todo = ({todo, removeTodo}) => {
+const Todo = ({todo, removeTodo,completeTodo}) => {
   return (
-    <div className="todo">
+    <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
         <div className="content">
             <p>{todo.text}</p>
             <p className="category">
@@ -10,7 +10,7 @@ const Todo = ({todo, removeTodo}) => {
             </p>
         </div>
         <div>
-            <button className='complete'>Completar</button>
+            <button className='complete' onClick={()=> completeTodo(todo.id)}>Completar</button>
             <button className='remove' onClick={()=> removeTodo(todo.id)}>Deletar</button>
         </div>
     </div>
